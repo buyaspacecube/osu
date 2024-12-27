f6f2783ce96c34ad05b0f20a48d598b83dafa008
Dean Herbert <pe@ppy.sh>
2024-12-27 21:36:07 +0900

    Remove duplicated block

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -1440,10 +1440,6 @@ namespace osu.Game
 
             switch (e.Action)
             {
-                case GlobalAction.DecreaseVolume:
-                case GlobalAction.IncreaseVolume:
-                    return volume.Adjust(e.Action);
-
                 case GlobalAction.ToggleMute:
                 case GlobalAction.NextVolumeMeter:
                 case GlobalAction.PreviousVolumeMeter:
