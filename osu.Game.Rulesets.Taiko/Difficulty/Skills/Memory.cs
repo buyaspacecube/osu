08a9911687fb0b7b3f8b0f9bc36dc424edaa30b0
Eloise <eloisesharpe2708@gmail.com>
2024-12-27 18:24:27 +0000

    Implement memory bonus for consecutive hard to read notes

--- osu.Game.Rulesets.Taiko/Difficulty/Skills/Memory.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Skills/Memory.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
@@ -18,9 +19,11 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Skills
     public class Memory : StrainDecaySkill
     {
         protected override double SkillMultiplier => 1.0;
-        protected override double StrainDecayBase => 0.88; // Slow decay as generally memorising more notes is harder
+        protected override double StrainDecayBase => 0.8;
 
         private double currentStrain;
+		
+		private double consecutiveHardToReadBonus; // wtf
 
         public Memory(Mod[] mods)
             : base(mods)
@@ -40,12 +43,19 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Skills
 			var colourDifficulty = ColourEvaluator.EvaluateDifficultyOf(taikoObject);      
 			
 			// Notes below this threshold are not worth memorising so give 0 memory difficulty
-			// Notes with the highest reading difficulty (1.5) need to be memorised so give 1 memory difficulty
+			// Notes give more memory difficulty up to 1 as they approach maximum reading difficulty
 			var hardToReadThreshold = 0.75;
 			var memoryDifficulty = DifficultyCalculationUtils.Logistic(readingDifficulty, (hardToReadThreshold + 1.5) / 2, 12);
-			
+
 			currentStrain *= StrainDecayBase;
-			currentStrain += memoryDifficulty * (colourDifficulty * 0.15) * SkillMultiplier;
+			currentStrain += memoryDifficulty * (colourDifficulty * 0.3) * consecutiveHardToReadBonus * SkillMultiplier;
+
+			// Small bonus to consecutive hard to read notes
+			// Aims to buff memorising longer sections
+			if (readingDifficulty >= hardToReadThreshold && consecutiveHardToReadBonus <= 1.25)
+			{
+				consecutiveHardToReadBonus += 0.01;
+			}
 
             return currentStrain;
         }
