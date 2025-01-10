fa343890604fb2a40450c89d7188685c75cf2b97
Eloise <eloisesharpe2708@gmail.com>
2025-01-10 13:06:50 +0000

    Reimplement VelocityRange

--- osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -9,6 +9,20 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
 {
     public static class ReadingEvaluator
     {
+		private readonly struct VelocityRange
+        {
+            public double Min { get; }
+            public double Max { get; }
+            public double Center => (Max + Min) / 2;
+            public double Range => Max - Min;
+
+            public VelocityRange(double min, double max)
+            {
+                Min = min;
+                Max = max;
+            }
+        }
+		
         /// <summary>
         /// Calculates the influence slider velocities on hitobject difficulty.
 		/// The bonus is determined based on the EffectiveBPM and "note density".
@@ -17,7 +31,7 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
         /// <returns>The reading difficulty value for the given hit object.</returns>
         public static double EvaluateDifficultyOf(TaikoDifficultyHitObject noteObject)
         {
-			// All curves and calculations can be found here https://www.desmos.com/calculator/rj6t8mik6b
+			// All curves and calculations can be found here https://www.desmos.com/calculator/xquxnvmo9k
             double effectiveBPM = Math.Max(1.0, noteObject.EffectiveBPM);
 			
 			// Expected deltatime is the deltatime this note would need
@@ -30,12 +44,16 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
 			double highDensityPenalty = DifficultyCalculationUtils.Logistic(density, 1.0, 9.0);
 			double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(density, 3.75, 4.5);
 			
-			double lowEndMidpointOffset = 400.0 + (120.0 * highDensityPenalty) - (200.0 * veryHighDensityBonus);
-			double lowEndMultiplier = (1.0 - 0.7 * veryHighDensityBonus) / 20.0;
+			// highEnd starts that low so its effect can apply slowly
+			var lowEnd = new VelocityRange(350, 450);
+			var highEnd = new VelocityRange(355, 605);
+			
+			double lowEndMidpointOffset = lowEnd.Center + (120.0 * highDensityPenalty) - (200.0 * veryHighDensityBonus);
+			double lowEndMultiplier = (5.0 - 3.5 * veryHighDensityBonus) / lowEnd.Range;
 			double lowEndDifficulty = DifficultyCalculationUtils.Logistic(effectiveBPM, lowEndMidpointOffset, lowEndMultiplier) * (1.0 + veryHighDensityBonus) + (2.0 * veryHighDensityBonus);
 			
-			double highEndMidpointOffset = 480.0 + (90.0 * highDensityPenalty);
-			double highEndMultiplier = 1.0 / 50.0;
+			double highEndMidpointOffset = highEnd.Center + (90.0 * highDensityPenalty);
+			double highEndMultiplier = 5.0 / highEnd.Range;
 			double highEndDifficulty = DifficultyCalculationUtils.Logistic(effectiveBPM, highEndMidpointOffset, highEndMultiplier) * (1.0 - Math.Pow(highDensityPenalty, 3));
 			
             return lowEndDifficulty * (0.3 + 1.2 * highEndDifficulty);
