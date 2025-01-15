b61e0e1564fe19e0b9f7a4f5474a64ba5d66ed74
Eloise <eloisesharpe2708@gmail.com>
2025-01-15 10:41:37 +0000

    Split density diff and high velocity diff into two different methods

--- osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -23,8 +23,28 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
             }
         }
 		
+		public static double evaluateHighVelocityDifficultyOf(double effectiveBPM, double density)
+		{
+			// Dense notes are penalised and very dense notes are rewarded
+			double highDensityPenalty = DifficultyCalculationUtils.Logistic(density, 1.0, 9.0);
+			double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(density, 4.0, 4.0);
+		
+			var highVelocity = new VelocityRange(270, 550);
+			
+			double thing = DifficultyCalculationUtils.Logistic(effectiveBPM, highVelocity.Center / (1.0 + 24.0 * veryHighDensityBonus), 5.0 * (1.0 - 0.45 * highDensityPenalty) * (1.0 + 2.0 * veryHighDensityBonus) / highVelocity.Range);
+			
+			return Math.Pow(thing, (2.0 * highDensityPenalty) + 2.5) * (1.0 - 0.75 * veryHighDensityBonus);
+		}
+		
+		public static double evaluateDensityDifficultyOf(double effectiveBPM, double density)
+		{
+			double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(density, 4.0, 4.0);
+			
+			return 0.75 * veryHighDensityBonus;
+		}
+		
         /// <summary>
-        /// Calculates the influence slider velocities on hitobject difficulty.
+        /// Calculates the influence of slider velocities on hitobject difficulty.
 		/// The bonus is determined based on the EffectiveBPM and "note density".
         /// </summary>
         /// <param name="noteObject">The hit object to evaluate.</param>
@@ -40,20 +60,10 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
 			// Density is the relation of this note's expected deltatime to its actual deltatime
 			double density = expectedDeltaTime / Math.Max(1.0, noteObject.DeltaTime);
 
-			// Dense notes are penalised and very dense notes are rewarded
-			double highDensityPenalty = DifficultyCalculationUtils.Logistic(density, 1.0, 9.0);
-			double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(density, 4.0, 4.0);
+			double highVelocityDifficulty = evaluateHighVelocityDifficultyOf(effectiveBPM, density);
+			double densityDifficulty = evaluateDensityDifficultyOf(effectiveBPM, density);
 			
-			var highVelocity = new VelocityRange(270, 550);
-			
-			double midpointOffset = highVelocity.Center / (1.0 + 24.0 * veryHighDensityBonus);
-			double multiplier = 5.0 * (1.0 - 0.45 * highDensityPenalty) * (1.0 + 2.0 * veryHighDensityBonus) / highVelocity.Range;
-			
-			double readingDifficulty = Math.Pow(DifficultyCalculationUtils.Logistic(effectiveBPM, midpointOffset, multiplier), 2.5 + (2.0 * highDensityPenalty))
-			* (1.0 - 0.75 * veryHighDensityBonus) + (0.75 * veryHighDensityBonus);
-			
-			readingDifficulty *= 1.5;
-			return readingDifficulty;
+			return (highVelocityDifficulty + densityDifficulty) * 1.5;
         }
     }
 }
