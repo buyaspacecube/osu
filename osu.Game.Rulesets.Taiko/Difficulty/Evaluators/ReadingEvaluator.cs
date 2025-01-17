4646e9e9efe0767b4a8cc061144ee48b2db6aabb
Eloise <eloisesharpe2708@gmail.com>
2025-01-17 11:01:03 +0000

    Implement Flashlight, clean up everything and probably change a few random numbers its whatever

--- osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -23,69 +23,68 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
             }
         }
 		
-		public static double evaluateLowVelocityDifficultyOf(double effectiveBPM, bool hasHidden)
-		{
-			if (hasHidden == false) return 0.0;
-			
-			var lowVelocity = new VelocityRange(120, 200);
-			
-			double lowVelocityDifficulty = 1.0 - DifficultyCalculationUtils.Logistic(effectiveBPM, lowVelocity.Center, 9.0 / lowVelocity.Range);
-			
-			return lowVelocityDifficulty;
-		}
-		
-		public static double evaluateHighVelocityDifficultyOf(double effectiveBPM, double density, bool hasHidden)
-		{
-			if (hasHidden)
-			{
-				var highVelocity = new VelocityRange(320, 500);
-				
-				double highDensityBonus = DifficultyCalculationUtils.Logistic(density, 3.0, 2.5);
-				
-				return (1.0 - highDensityBonus) * DifficultyCalculationUtils.Logistic(effectiveBPM, highVelocity.Center / (1.0 + 0.3 * highDensityBonus), 4.0 * (1.0 + 0.5 * highDensityBonus) / highVelocity.Range);
-			}
-			else
-			{
-				var highVelocity = new VelocityRange(270, 550);
-				
-				double highDensityPenalty = DifficultyCalculationUtils.Logistic(density, 1.0, 9.0);
-				double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(density, 4.0, 4.0);
-				
-				double thing = DifficultyCalculationUtils.Logistic(effectiveBPM, highVelocity.Center / (1.0 + 24.0 * veryHighDensityBonus), 5.0 * (1.0 - 0.45 * highDensityPenalty) * (1.0 + 2.0 * veryHighDensityBonus) / highVelocity.Range);
-			
-				return Math.Pow(thing, (2.0 * highDensityPenalty) + 2.5) * (1.0 - 0.75 * veryHighDensityBonus);
-			}
-		}
-		
-		public static double evaluateDensityDifficultyOf(double effectiveBPM, double density)
-		{
-			double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(density, 4.0, 4.0);
-			
-			return 0.75 * veryHighDensityBonus;
-		}
-		
         /// <summary>
         /// Calculates the influence of slider velocities on hitobject difficulty.
 		/// The bonus is determined based on the EffectiveBPM and "note density".
         /// </summary>
         /// <param name="noteObject">The hit object to evaluate.</param>
-		/// <param name="hasHidden">Whether or not the Hidden mod is enabled</param>
+		/// <param name="hasHidden">Whether or not the Hidden mod is enabled.</param>
+		/// <param name="hasFlashlight">Whether or not the Flashlight mod is enabled.</param>
         /// <returns>The reading difficulty value for the given hit object.</returns>
-        public static double EvaluateDifficultyOf(TaikoDifficultyHitObject noteObject, bool hasHidden)
+        public static double EvaluateDifficultyOf(TaikoDifficultyHitObject noteObject, bool hasHidden, bool hasFlashlight)
         {
+			// Until memory considerations are added, all notes give 1.5 reading difficulty with HDFL
+			if (hasHidden && hasFlashlight)
+			{
+				return 1.5;
+			}
+			
             double effectiveBPM = Math.Max(1.0, noteObject.EffectiveBPM);
 			
 			// Expected deltatime is the deltatime this note would need
 			// to be spaced equally to a base SV 1/4 note at this effective BPM
 			double expectedDeltaTime = 21000.0 / effectiveBPM;
 			// Density is the relation of this note's expected deltatime to its actual deltatime
-			double density = expectedDeltaTime / Math.Max(1.0, noteObject.DeltaTime);
+			double objectDensity = expectedDeltaTime / Math.Max(1.0, noteObject.DeltaTime);
 
-			double lowVelocityDifficulty = evaluateLowVelocityDifficultyOf(effectiveBPM, hasHidden);
-			double highVelocityDifficulty = evaluateHighVelocityDifficultyOf(effectiveBPM, density, hasHidden);
-			double densityDifficulty = evaluateDensityDifficultyOf(effectiveBPM, density);
+			var lowVelocity = new VelocityRange(10, 150);
+			var highVelocity = new VelocityRange(240, 550);
+
+			// All curves can be found here https://www.desmos.com/calculator/0a5evgsj5f
+			// Stay tuned for comments actually explaining all this
+			double readingDifficulty;
 			
-			return (lowVelocityDifficulty + highVelocityDifficulty + densityDifficulty) * 1.5;
+			if (hasHidden)
+			{
+				double hdHighDensityBonus = DifficultyCalculationUtils.Logistic(objectDensity, 3.0, 2.5);
+				
+				double hdLowVelocityDifficulty = 1.0 - DifficultyCalculationUtils.Logistic(effectiveBPM, lowVelocity.Center * (1.0 + 2.0 * hdHighDensityBonus), 10.0 / lowVelocity.Range);
+				double hdHighVelocityDifficulty = Math.Pow(DifficultyCalculationUtils.Logistic(1.2 * effectiveBPM, highVelocity.Center, 5.0 * (1.0 + hdHighDensityBonus) / highVelocity.Range), 3.0);
+				
+				readingDifficulty = (1.0 - hdHighDensityBonus) * (hdLowVelocityDifficulty + hdHighVelocityDifficulty) + hdHighDensityBonus;
+			}
+			
+			else if (hasFlashlight)
+			{
+				double flVeryHighDensityBonus = DifficultyCalculationUtils.Logistic(objectDensity, 4.0, 4.0);
+				double flBonusNormalised = 0.75 * Math.Pow(flVeryHighDensityBonus, 0.8);
+				
+				double flHighVelocityDifficulty = Math.Pow(DifficultyCalculationUtils.Logistic(2.0 * effectiveBPM, highVelocity.Center / (1.0 + flVeryHighDensityBonus), 5.0 / highVelocity.Range), 2.5);
+				
+				readingDifficulty = (1.0 - flBonusNormalised) * flHighVelocityDifficulty + flBonusNormalised;
+			}
+			
+			else
+			{
+				double highDensityPenalty = DifficultyCalculationUtils.Logistic(objectDensity, 1.0, 9.0);
+				double veryHighDensityBonus = DifficultyCalculationUtils.Logistic(objectDensity, 4.0, 4.0);
+				
+				double highVelocityDifficulty = Math.Pow(DifficultyCalculationUtils.Logistic(effectiveBPM, (highVelocity.Center / (1.0 + 3.0 * veryHighDensityBonus)) + (120.0 * highDensityPenalty), 5.0 / highVelocity.Range), (2.5 - highDensityPenalty));
+
+				readingDifficulty = (1.0 - 0.75 * veryHighDensityBonus) * highVelocityDifficulty + (0.75 * veryHighDensityBonus);
+			}
+			
+			return readingDifficulty * 1.5;
         }
     }
 }
