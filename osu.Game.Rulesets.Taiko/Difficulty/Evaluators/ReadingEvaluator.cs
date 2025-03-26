ce1af3ad0abd8e319a74444f48f10ea5bc9175b4
Eloise <eloisesharpe2708@gmail.com>
2025-03-26 13:51:10 +0000

    Adjust curves again to try and save hr

--- osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -53,21 +53,24 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
 
         public static double EvaluateVelocityDifficultyOf(double effectiveBPM, double objectDensity, bool hasHidden, bool hasFlashlight)
         {
+            // Effective BPM is multiplied with hidden and flashlight to reflect notes being visible for less time
+            if (hasHidden) effectiveBPM *= 1.4;
+            if (hasFlashlight) effectiveBPM *= 3.0;
+
             double velocityDifficulty = 0.0;
 
             // With hidden, notes at low velocities are hard to read
-            var lowVelocity = new VelocityRange(70, 250);
+            var lowVelocity = new VelocityRange(100, 350);
             velocityDifficulty += (hasHidden) ? 1.0 - DifficultyCalculationUtils.Logistic(effectiveBPM, lowVelocity.Center, 10.0 / lowVelocity.Range) : 0.0;
 
             // Without hidden, notes at high velocities are generally harder to read with lower object density (think HR streams vs DT)
             // To reflect this, the high velocity range is shifted based on object density
-            double lowDensityBonus = (hasHidden) ? 1.0 : DifficultyCalculationUtils.Logistic(objectDensity, 0.72, -25);
+            double lowDensityBonus = (hasHidden) ? 1.0 : 1.0 - DifficultyCalculationUtils.Logistic(objectDensity, 0.7, 25);
 
-            var highVelocity = new VelocityRange(420 - (140 * lowDensityBonus), 800);
-
-            // Effective BPM is multiplied with hidden and flashlight to reflect notes being visible for less time
-            if (hasHidden) effectiveBPM *= 1.4;
-            if (hasFlashlight) effectiveBPM *= 3.0;
+            var highVelocity = new VelocityRange(
+                420 - (90 * lowDensityBonus), 
+                840 - (50 * lowDensityBonus)
+            );
 
             velocityDifficulty += DifficultyCalculationUtils.Logistic(effectiveBPM, highVelocity.Center, 10.0 / highVelocity.Range);
 
